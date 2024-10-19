e90a998712feef05c0931f35d133b362a036b7ae
Ira <wilyplus@gmail.com>
2024-10-19 22:23:53 +0300

    Add localization

--- app/src/main/java/ru/netology/coinapp/ui/AssetListScreen.kt
+++ app/src/main/java/ru/netology/coinapp/ui/AssetListScreen.kt
@@ -1,8 +1,14 @@
 package ru.netology.coinapp.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,11 +16,14 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ru.netology.coinapp.R
 import ru.netology.coinapp.dto.Asset
 import ru.netology.coinapp.viewmodel.AssetViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AssetListScreen(
     viewModel: AssetViewModel,
@@ -27,9 +36,22 @@ fun AssetListScreen(
     }
 
     LazyColumn {
-        items(assets!!) { item ->
-            Surface(onClick = { onAssetDetails(item) }) {
-                Text( item.name, Modifier.padding(24.dp) )
+        items(assets) { item ->
+            ElevatedCard(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+                onClick = { onAssetDetails(item) }
+            ) {
+                Text(
+                    text = item.name,
+                    modifier = Modifier.padding(24.dp)
+                )
+                Row {
+                    Text(stringResource(R.string.asset_supply))
+                    Text(item.supply.toString())
+                }
+
             }
         }
     }
